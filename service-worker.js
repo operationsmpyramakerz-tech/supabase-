0f73b14419cd69c13d8b03e1c9933bb073e03836
Abdullah EL-Sayed <operations.m.pyramakerz@gmail.com>
2026-05-05 23:43:11 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Bump this value whenever we change static assets (CSS/JS/images)
 // so existing installs don't keep serving stale cached files.
-const CACHE_NAME = "ops-static-v15";
+const CACHE_NAME = "ops-static-v16";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
