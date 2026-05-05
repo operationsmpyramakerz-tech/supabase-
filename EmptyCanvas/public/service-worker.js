ca5fedb20275c25723f8fea4ab5dde15027eaf06
Abdullah EL-Sayed <operations.m.pyramakerz@gmail.com>
2026-05-05 23:43:52 +0300

    Add files via upload

--- EmptyCanvas/public/service-worker.js
+++ EmptyCanvas/public/service-worker.js
@@ -1,6 +1,6 @@
 // Bump this value whenever we change static assets (CSS/JS/images)
 // so existing installs don't keep serving stale cached files.
-const CACHE_NAME = "ops-static-v25";
+const CACHE_NAME = "ops-static-v26";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
