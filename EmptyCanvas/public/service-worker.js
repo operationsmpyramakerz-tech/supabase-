71cdeb671599bb6546c5c0290ad01e901b7cae58
Abdullah EL-Sayed <operations.m.pyramakerz@gmail.com>
2026-05-16 02:18:25 +0300

    Add files via upload

--- EmptyCanvas/public/service-worker.js
+++ EmptyCanvas/public/service-worker.js
@@ -1,6 +1,6 @@
 // Operations Hub PWA Service Worker
 // Bump this value whenever we change static assets so old deployments don't stay cached.
-const CACHE_NAME = "ops-static-vv61-mobile-normal-page";
+const CACHE_NAME = "ops-static-vv58-mobile-measured-scroll";
 
 const PRECACHE_URLS = [
   "/pwa-start",
